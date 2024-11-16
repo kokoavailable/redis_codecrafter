3fcd2c6f09b2d27ec12bb20b7906c939a1a03cf2
Monsangter <sangdobae@icloud.com>
2024-11-16 21:10:27 +0900

    [any message]

--- app/main.py
+++ app/main.py
@@ -7,8 +7,10 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
+
+    # 포트 이미 사용중. 오류 발생
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.accept() # wait for client
 
 
 if __name__ == "__main__":
