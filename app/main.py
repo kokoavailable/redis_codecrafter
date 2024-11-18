ab378c59a0310ea874892919d08a74e6331b140a
Monsangter <sangdobae@icloud.com>
2024-11-19 08:26:33 +0900

    [any message]

--- app/main.py
+++ app/main.py
@@ -10,8 +10,20 @@ def main():
 
     # 포트 이미 사용중. 오류 발생
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
+    client_socket, _ = server_socket.accept()
+    
+    while True:
+        request: bytes = client_socket.recv(512)
+        data: str = request.decode()
+
+        if "ping" in data.lower():
+            client_socket.sendall("+PONG\r\n".encode())
+    # while True:
+    #     request: bytes = client_socket.recv(512)
+    #     data: str = request.decode()
+
+    #     if "ping" in data.lower():
+    #         client_socket.send("+PONG\r\n".encode())
 
 
 if __name__ == "__main__":
